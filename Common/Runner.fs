35dee2c8093df972e16fa8622355c9803787cb0b
fluxehub <53355311+fluxehub@users.noreply.github.com>
2024-12-01 16:08:50 +0000

    Day 1 solutions and framework

---
+++ Common/Runner.fs
@@ -0,0 +1,51 @@
+﻿namespace Common
+
+open FsHttp
+
+type Solution<'i, 'a> = 'i -> 'a
+
+type AocDay<'i, 'a, 'b> =
+    { Day: int
+      Part1: Solution<'i, 'a> option
+      Part2: Solution<'i, 'b> option }
+
+module Runner =
+    type AocDayBuilder() =
+        member _.Yield(()) = ()
+
+        [<CustomOperation("day")>]
+        member _.Day((), day) =
+            { Day = day
+              Part1 = None
+              Part2 = None }
+
+        [<CustomOperation("part1")>]
+        member _.Part1(day, solution) = { day with Part1 = Some solution }
+
+        [<CustomOperation("part2")>]
+        member _.Part2(day, solution) = { day with Part2 = Some solution }
+
+    let aoc = AocDayBuilder()
+
+    let private readSessionToken = System.IO.File.ReadAllText "session"
+
+    let private getDayInput day =
+        http {
+            GET $"https://adventofcode.com/2024/day/{day}/input"
+            Cookie "session" readSessionToken
+        }
+        |> Request.send
+        |> Response.toString None
+        |> _.TrimEnd('\n') // Strip trailing newline
+
+    let private runSolution input solution =
+        solution |> Option.map (fun solution -> solution input)
+
+    let run inputProcessor day =
+        let input = inputProcessor <| getDayInput day.Day
+
+        runSolution input day.Part1
+        |> Option.iter (fun answer -> (printfn $"Part 1: {answer}"))
+
+        runSolution input day.Part2
+        |> Option.iter (fun answer -> (printfn $"Part 2: {answer}"))
---
+++ Day1/Program.fs
@@ -0,0 +1,30 @@
+﻿open Common.Runner
+
+let splitInput (input: string) =
+    input.Split("\n")
+    |> Array.fold
+        (fun (leftList, rightList) line ->
+            let values = line.Split("   ")
+            let left = int values[0]
+            let right = int values[1]
+            left :: leftList, right :: rightList)
+        ([], [])
+
+let findTotalDistanceSum (left, right) =
+    let left = List.sort left
+    let right = List.sort right
+
+    List.zip left right |> List.map (fun (a, b) -> abs (a - b)) |> List.sum
+
+let findTotalSimilaritySum (left, right) =
+    let appearances = right |> List.countBy id |> Map.ofList
+
+    left
+    |> List.sumBy (fun v -> v * (appearances |> Map.tryFind v |> Option.defaultValue 0))
+
+aoc {
+    day 1
+    part1 findTotalDistanceSum
+    part2 findTotalSimilaritySum
+}
+|> run splitInput
