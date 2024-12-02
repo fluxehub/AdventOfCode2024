da55f4f20a81d723b6d2e7124e06ac667c67e16e
fluxehub <53355311+fluxehub@users.noreply.github.com>
2024-12-02 13:38:24 +0000

    Added brute force solution

--- Day2/Program.fs
+++ Day2/Program.fs
@@ -1,39 +1,55 @@
 ﻿open Common.Runner
 open FSharpPlus
 
-let isSafe differences =
+// I originally found a smart non-brute force method but I don't like the code
+// Keeping it here anyway because solving it this way caused me great pain
+// and I need recognition for my sacrifice
+
+// let isSafeDifferences differences =
+//     List.forall (fun v -> sign v = sign (List.head differences) && abs v > 0 && abs v <= 3) differences
+//
+// let removeRecord index differences =
+//     if index = 0 then
+//         List.tail differences
+//     elif index = List.length differences then
+//         differences |> List.deleteAt (index - 1)
+//     else
+//         differences
+//         |> List.updateAt index (differences[index - 1] + differences[index])
+//         |> List.deleteAt (index - 1)
+//
+// let canRemove differences predicate =
+//     let matches = differences |> List.map predicate
+//     let firstUnsafe = List.findIndex not matches
+//
+//     isSafeDifferences (removeRecord firstUnsafe differences)
+//     || isSafeDifferences (removeRecord (firstUnsafe + 1) differences)
+//
+// let countSafeWithDampener reports =
+//     reports
+//     |> List.map findDifferences
+//     |> List.filter (fun d ->
+//         isSafeDifferences d
+//         || canRemove d (fun x -> -3 <= x && x < 0) // Can't reuse predicate because of edge case
+//         || canRemove d (fun x -> 0 < x && x <= 3))
+//     |> List.length
+
+let isSafe (report: int list) =
+    let differences =
+        List.map2 (-) (List.take (List.length report - 1) report) (List.tail report)
+
     List.forall (fun v -> sign v = sign (List.head differences) && abs v > 0 && abs v <= 3) differences
 
-let removeRecord index differences =
-    if index = 0 then
-        List.tail differences
-    elif index = List.length differences then
-        differences |> List.deleteAt (index - 1)
-    else
-        differences
-        |> List.updateAt index (differences[index - 1] + differences[index])
-        |> List.deleteAt (index - 1)
-
-let canRemove differences predicate =
-    let matches = differences |> List.map predicate
-    let firstUnsafe = List.findIndex not matches
-
-    isSafe (removeRecord firstUnsafe differences)
-    || isSafe (removeRecord (firstUnsafe + 1) differences)
-
 let countSafeReports reports =
-    reports
-    |> List.map (fun report -> List.map2 (-) (List.take (List.length report - 1) report) (List.tail report))
-    |> List.filter isSafe
-    |> List.length
+    reports |> List.filter isSafe |> List.length
 
-let countSafeWithDampener reports =
+let countSafeWithDampenerBrute reports =
     reports
-    |> List.map (fun report -> List.map2 (-) (List.take (List.length report - 1) report) (List.tail report))
-    |> List.filter (fun d ->
-        isSafe d
-        || canRemove d (fun x -> -3 <= x && x < 0) // Can't reuse predicate because of edge case
-        || canRemove d (fun x -> 0 < x && x <= 3))
+    |> List.filter (fun report ->
+        isSafe report
+        || seq { 0 .. (List.length report) - 1 }
+           |> Seq.map (fun i -> report |> List.deleteAt i |> isSafe)
+           |> Seq.contains true)
     |> List.length
 
 aoc {
@@ -46,5 +62,5 @@ aoc {
         |> Seq.toList)
 
     part1 countSafeReports
-    part2 countSafeWithDampener
+    part2 countSafeWithDampenerBrute
 }
