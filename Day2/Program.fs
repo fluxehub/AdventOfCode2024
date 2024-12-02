9b76660706fd8bc1c09edfa96c509cb3aa53ea22
fluxehub <53355311+fluxehub@users.noreply.github.com>
2024-12-02 10:37:14 +0000

    Day 2 ugly solution

---
+++ Day2/Program.fs
@@ -0,0 +1,57 @@
+﻿open Common.Runner
+open FSharpPlus
+
+let safePredicate report =
+    (List.forall ((>) 0) report || List.forall ((<) 0) report)
+    && List.forall (abs >> (>=) 3) report
+
+let removeRecord index differences =
+    if index = 0 then
+        List.tail differences
+    elif index = List.length differences then
+        differences |> List.deleteAt (index - 1)
+    else
+        differences
+        |> List.updateAt index (differences[index - 1] + differences[index])
+        |> List.deleteAt (index - 1)
+
+let canRemove differences predicate =
+    let matches = differences |> List.map predicate
+    let firstUnsafe = List.findIndex not matches
+
+    safePredicate (removeRecord firstUnsafe differences)
+    || safePredicate (removeRecord (firstUnsafe + 1) differences)
+
+let countSafeReports reports =
+    reports
+    |> List.map (fun report ->
+        (List.head report, List.tail report)
+        ||> List.mapFold (fun last level -> level - last, level)
+        |> fst)
+    |> List.filter safePredicate
+    |> List.length
+
+let countSafeWithDampener reports =
+    reports
+    |> List.map (fun report ->
+        (List.head report, List.tail report)
+        ||> List.mapFold (fun last level -> level - last, level)
+        |> fst)
+    |> List.filter (fun d ->
+        safePredicate d
+        || canRemove d (fun x -> -3 <= x && x < 0)
+        || canRemove d (fun x -> 0 < x && x <= 3))
+    |> List.length
+
+aoc {
+    day 2
+
+    inputTransformer (fun input ->
+        input
+        |> String.split [ "\n" ]
+        |> Seq.map (String.split [ " " ] >> Seq.map int >> Seq.toList)
+        |> Seq.toList)
+
+    part1 countSafeReports
+    part2 countSafeWithDampener
+}
