623c8fac9ec610acdbce6c8ee8cc70f521d001dd
fluxehub <53355311+fluxehub@users.noreply.github.com>
2024-12-03 08:52:20 +0000

    Day 3 solution

---
+++ Day3/Day3.fs
@@ -0,0 +1,17 @@
+﻿open System.Text.RegularExpressions
+open Common.Runner
+open FSharpPlus
+
+let sumOfMul input =
+    Regex.Matches(input, "mul\((\d+),(\d+)\)")
+    |> Seq.sumBy (fun m -> int m.Groups[1].Value * int m.Groups[2].Value)
+
+aoc {
+    day 3
+
+    part1 sumOfMul
+
+    part2 (fun input ->
+        Regex.Replace(String.replace "\n" "" input, "don't\(\).+?do\(\)|don't\(\).+", "")
+        |> sumOfMul)
+}
