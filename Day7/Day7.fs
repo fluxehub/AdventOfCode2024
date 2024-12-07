5bd97c9ac7547e9623119915237b8eb1b19fa51d
fluxehub <53355311+fluxehub@users.noreply.github.com>
2024-12-07 11:59:38 +0000

    Day 7

---
+++ Day7/Day7.fs
@@ -0,0 +1,25 @@
+﻿#nowarn 0025
+
+open Common
+
+// I don't understand how this works and trust me I tried to
+let allComb l1 l2 =
+    List.foldBack (fun x acc -> l1 |> List.collect (fun y -> acc |> List.map (fun rest -> (y, x) :: rest))) l2 [ [] ]
+
+let canEvaluateToTarget operators (target, start :: rest) =
+    let evaluateList = List.fold (fun acc (op, value) -> op acc value) start
+
+    allComb operators rest |> List.map evaluateList |> List.contains target
+
+let (++) a b = string a + string b |> int64
+
+aoc {
+    day 7
+
+    mapLine (fun line ->
+        let [ target; values ] = String.splitList ":" line
+        int64 target, values |> String.splitList " " |> List.tail |> List.map int64)
+
+    part1 (List.filter (canEvaluateToTarget [ (+); (*) ]) >> List.map fst >> List.sum)
+    part2 (List.filter (canEvaluateToTarget [ (+); (*); (++) ]) >> List.map fst >> List.sum)
+}
